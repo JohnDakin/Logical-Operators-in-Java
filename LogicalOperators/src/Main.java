56b410b03b521ebf0270e4236260e44db67d456d
John Dakin <jdakin749@gmail.com>
2024-10-30 11:34:36 +0300

    Add files via upload

---
+++ LogicalOperators/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("You are playing a game! press q or Q to quit");
+		String response  = scanner.next();
+		
+		if(!response.equals("q") && !response.equals("Q")) {
+			System.out.println("You are still playing the game *pew pew*");
+		}
+		else {
+			
+			System.out.println("You quit the game");
+		}
+	}
+
+}
